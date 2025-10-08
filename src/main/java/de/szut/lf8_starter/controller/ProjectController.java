390162c45e7711d34e5529288e8670cbac1fb8e6
Henrik <hh27dev@gmail.com>
2025-10-08 09:03:19 +0200

    DP-10: Added controller and service method to get all projects

--- src/main/java/de/szut/lf8_starter/controller/ProjectController.java
+++ src/main/java/de/szut/lf8_starter/controller/ProjectController.java
@@ -3,9 +3,15 @@ package de.szut.lf8_starter.controller;
 import de.szut.lf8_starter.dtos.create.CreateProjectDto;
 import de.szut.lf8_starter.dtos.get.GetProjectDto;
 import de.szut.lf8_starter.services.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/projects")
+@Tag(name = "Projektverwaltung", description = "API zur Verwaltung von Projekten")
 public class ProjectController {
 
   private final ProjectService projectService;
@@ -22,10 +29,26 @@ public class ProjectController {
   }
 
 
+
+  @Operation(summary = "Erstellt ein neues Projekt")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Projekt erfolgreich erstellt"),
+      @ApiResponse(responseCode = "400", description = "Ungültige Eingabedaten")
+  })
   @PostMapping
   public ResponseEntity<GetProjectDto> createProject(@Valid @RequestBody CreateProjectDto createProjectDto) {
     GetProjectDto createProject = projectService.createProject(createProjectDto);
     return new ResponseEntity<>(createProject, HttpStatus.CREATED);
+  }
+
+
+  @Operation(summary = "Gibt alle Projekte zurück")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Liste aller Projekte")
+  })
+  @GetMapping
+  public ResponseEntity<List<GetProjectDto>> getAllProjects() {
+    return ResponseEntity.status(HttpStatus.OK).body(projectService.getAllProjects());
   }
 
 }
